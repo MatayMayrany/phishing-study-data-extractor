a05e18737b5b08a228ab3ff970c410989b0aa389
Matay Mayrany <mmayrany@invidi.com>
2022-12-20 17:25:18 +0100

    first commit

---
+++ model/result.py
@@ -0,0 +1,55 @@
+from dataclasses import dataclass
+from typing import Any
+
+@dataclass
+class ClicksByDay:
+    x: int
+    y: int
+
+    @staticmethod
+    def from_dict(obj: Any) -> 'ClicksByDay':
+        _x = int(obj.get("x"))
+        _y = int(obj.get("y"))
+        return ClicksByDay(_x, _y)
+
+@dataclass
+class Result:
+    title: str
+    date: str
+    category: str
+    groups: str
+    recipients: int
+    deliveries: int
+    clicks: int
+    attachmentOpened: int
+    dataEntered: int
+    otherFailures: int
+    pabReported: int
+    bounces: int
+    frm: str
+    subject: str
+    attachment: str
+    phishDomain: str
+    clicksByDay: dict
+
+    @staticmethod
+    def from_dict(obj: Any) -> 'Result':
+        _title = str(obj.get("title"))
+        _date = str(obj.get("date"))
+        _category = str(obj.get("category"))
+        _groups = str(obj.get("groups"))
+        _recipients = int(obj.get("recipients"))
+        _deliveries = int(obj.get("deliveries"))
+        _clicks = int(obj.get("clicks"))
+        _attachmentOpened = int(obj.get("attachmentOpened"))
+        _dataEntered = int(obj.get("dataEntered"))
+        _otherFailures = int(obj.get("otherFailures"))
+        _pabReported = int(obj.get("pabReported"))
+        _bounces = int(obj.get("bounces"))
+        _frm = str(obj.get("frm"))
+        _subject = str(obj.get("subject"))
+        _attachment = str(obj.get("attachment"))
+        _phishDomain = str(obj.get("phishDomain"))
+        _clicksByDay = dict(obj.get("clicksByDay"))
+        return Result(_title, _date, _category, _groups, _recipients, _deliveries, _clicks, _attachmentOpened, _dataEntered, _otherFailures, _pabReported, _bounces, _frm, _subject, _attachment, _phishDomain, _clicksByDay)
+
---
+++ scripts/data-extractor.py
@@ -0,0 +1,19 @@
+import json
+import pandas as pd
+from model.result import Result
+import os
+
+results_dir = 'campaign-results'
+
+
+def main():
+    for results_file in reversed(os.listdir(results_dir)):
+        results_file_path = os.path.join(results_dir, results_file)
+        with open(results_file_path) as rd:
+            datapoint = json.loads(rd.read())
+            result = Result.from_dict(datapoint)
+            print(result)
+
+
+if __name__ == "__main__":
+    main()
